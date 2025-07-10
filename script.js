376e658997ae9770213382c4a5aeff3c2da237a1
cesar-lima <cesarr340@gmail.com>
2025-07-09 23:35:50 -0300

    integrar api do gemini com um prompt estruturado, adição de biblioteca para formatar a resposta da api e modificações no css para melhor visualização dos componentes

--- script.js
+++ script.js
@@ -5,12 +5,69 @@ const askButton = document.getElementById("askButton");
 const response = document.getElementById("response");
 const form = document.getElementById("form");
 
-const aiQuestion = async (question, game, apiKey) => {
-    const model = "gemini-2.0-flash";
-    const url = ``;
+const MarkdownToHTML = (text) => {
+    const converter = new showdown.Converter();
+    return converter.makeHtml(text);
 }
 
-const enviarFormulario = (event) => {
+const aiQuestion = async (question, game, apiKey) => {
+    const model = "gemini-2.0-flash";
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
+    const questionData = `
+        ## Especialidade
+        Você é um especialista em ser um assistente de meta para o jogo ${game}
+
+        ## Tarefa
+        Você deve responder a pergunta do usuário com base no seu conhecimento sobre o jogo, estratégias, buils e dicas
+
+        ## Regras
+        - Se você não souber a resposta, responda com 'Desculpe, não sei a resposta para essa pergunta.' e não tente inventar uma resposta
+        - Se a pergunta não está relacionada ao jogo, responda com 'Desculpe, esta pergunta não está relacionda ao jogo.'
+        - considere a data atual ${new Date().toLocaleDateString()}
+        - faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta correta
+        - nunca responda itens, builds ou estratégias que você não tenha certeza de que existe no patch atual
+
+        ## Resposta
+        - Economize na resposta, seja direto e objetivo, responda apenas o necessário
+        - Responda em markdown
+        - caso na resposta tenha algum termo em inglês, traduzir para o português
+        - Não precisa fazer nenhuma saudação, despedida ou agradecimento, apenas responda a pergunta do usuário
+
+        ## Exemplo de resposta
+        Pergunta do usuário: Qual é a melhor build para o campeão X no patch atual?
+
+        Resposta: A melhor build para o campeão X no patch atual é:\n\n **itens:**\n\n coloque os itens aqui\n\n **runas:**\n\n coloque as runas aqui\n\n **estratégia:**\n\n coloque a estratégia aqui\n\n
+        ___
+
+        aqui está a pergunta do usuário: ${question}
+    `
+    const contents = [{
+        role: "user",
+        parts: [{
+            text: questionData
+        }]
+    }]
+
+    const tools = [{
+        google_search: {}
+    }]
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            contents,
+            tools
+        })
+    })
+
+    const responseData = await response.json();
+    return responseData.candidates[0].content.parts[0].text;
+}
+
+const enviarFormulario = async (event) => {
     event.preventDefault();
 
     const apiKey = apiKeyInput.value;
@@ -27,16 +84,16 @@ const enviarFormulario = (event) => {
     askButton.classList.add("loading");
 
     try {
-        aiQuestion(question, game, apiKey)
+        const text = await aiQuestion(question, game, apiKey)
+        response.querySelector('.response-content').innerHTML = MarkdownToHTML(text);
+        response.classList.remove("hidden");
     } catch (error) {
-        alert('Ocorreu um erro', error)
+        alert('Ocorreu um erro')
     } finally {
         askButton.disabled = false;
         askButton.textContent = "Perguntar";
         askButton.classList.remove("loading");
     }
-
-
 }
 
 form.addEventListener('submit', enviarFormulario);
